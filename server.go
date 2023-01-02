ed0a295e44030244c47718cfb93ece96630d6c24
Klao Alamus <pklaoalamus@gmail.com>
2023-01-02 17:46:36 +0700

    Add Echo to go.mod

--- server.go
+++ server.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
+func Handler(c echo.Context) error {
+	fmt.Println("inside handler")
+	return c.JSON(http.StatusOK, "hello world")
+}
+
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	// fmt.Println("Please use server.go for main file")
+	// fmt.Println("start at port:", os.Getenv("PORT"))
+
+	e := echo.New()
+
+	e.GET("/users", Handler)
+
 }
