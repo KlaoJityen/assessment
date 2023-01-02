1dde31526ba6bb777f51d42281e2edbef8fc6d70
Klao Alamus <pklaoalamus@gmail.com>
2023-01-02 23:47:53 +0700

    Add middleware

--- server.go
+++ server.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/KlaoJityen/assessment/expense"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+func VerifyAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Request().Header.Get("Authorization")
+		if token != "November 10, 2009" {
+			return c.JSON(http.StatusUnauthorized, "Unauthorized Access")
+		}
+		return next(c)
+	}
+}
 
 func main() {
 
 	expense.InitDB()
 
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(VerifyAuth)
 
 	e.GET("/expenses", expense.GetExpensesHandler)
 	e.GET("/expenses/:id", expense.GetExpenseHandler)
